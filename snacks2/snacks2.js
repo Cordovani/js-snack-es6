8fa6ed9dae6fbf8b7c8de960c935229e84ec527a
Cordovani <lor.cordovani@gmail.com>
2021-05-03 15:33:34 +0200

    funzione per filtrare le squadre

--- snacks2/snacks2.js
+++ snacks2/snacks2.js
@@ -1,10 +1,7 @@
 // Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0. Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
-// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
-
 
 //array delle squadre 
-
-var arraySquadre = [
+var squadre = [
     {
         nome: "Roma",
         punti_fatti: 0,
@@ -16,7 +13,7 @@ var arraySquadre = [
         punti_fatti: 0,
         falli_subiti: 0
     },
-    
+
     {
         nome: "Inter",
         punti_fatti: 0,
@@ -25,6 +22,34 @@ var arraySquadre = [
 
 ]
 
-function genRNDstats(){
+// genero le statistiche random 
+function genRndStats(listaSquadre) {
+
 
 }
+
+// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+function creaSquadreFiltrate(listaSquadre) {
+    const squadreFiltrate = [];
+
+    for (const squadraCorrente of listaSquadre) {
+        const { nome, falli_subiti } = squadraCorrente;
+       
+        squadreFiltrate.push({
+            nome,
+            falli_subiti
+        });
+    }
+
+    return squadreFiltrate;
+}
+
+
+
+
+// CORPO DEL PROGRAMMA 
+const nuoveSquadre = creaSquadreFiltrate(squadre);
+
+console.log(squadre);
+console.log(nuoveSquadre);
+
